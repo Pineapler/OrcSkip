493130701f65568a69aa1edabd4a91e3dc836c87
Pineapler <pineaplers@gmail.com>
2024-02-18 01:09:41 +1100

    Bump release version

--- OrcSkip/Plugin.cs
+++ OrcSkip/Plugin.cs
@@ -11,7 +11,7 @@ public class Plugin : BaseUnityPlugin {
     // GAME CONFIGURATION
     public const string PluginGuid = "com.pineapler.orcskip";
     public const string PluginName = "OrcSkip";
-    public const string PluginVersion = "0.0.1";
+    public const string PluginVersion = "1.0.0";
     // ==========================================================
 
     public static new Config Config { get; private set; }
